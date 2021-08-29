8b7111b1cda68f94bc342b64853f3eff52843ba0
sgfk26 <fuchi826.com>
2021-08-30 02:21:53 +0900

    あ

--- src/pages/P1Emerald.js
+++ src/pages/P1Emerald.js
@@ -1,6 +1,6 @@
 import React from "react";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import { red } from "@material-ui/core/colors";
+
 
 const styleCss = makeStyles({
   root: {

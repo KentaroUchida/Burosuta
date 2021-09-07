e99b41c5be44ac5379f24630fa293522a7bfa6aa
KentaroUchida <219x003x@stu.kobe-u.ac.jp>
2021-09-08 00:15:05 +0900

    home修正

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,11 +12,17 @@ const styleCss = makeStyles((theme) =>({
     width : "100%",
     height : "100%",
     background: "linear-gradient(#ffffff,#febe20)",
-    padding : theme.spacing(5),
+    padding : theme.spacing(4),
   },
   title: {
-    variant:"h4",
-    align: "center",
+    textShadow: "0px 2px 3px #808080",
+  },
+  appIntro: {
+    padding : theme.spacing(2),
+    width : "250px",
+  },
+  appIntrotitle: {
+    textShadow: "0px 2px 3px #808080",
   }
 }))
 
@@ -24,33 +30,31 @@ function Page() {
     const classes = styleCss();
     return (
       <body className={classes.root}>
-        <header></header>
-        <main>
+        <header>
           <ResponsiveFontProvider>
-            <Typography variant="h4" align="center">ブロスタ完全攻略</Typography>
+            <Typography variant="h4" align="center" className={classes.title}>ブロスタ完全攻略</Typography>
           </ResponsiveFontProvider>
-          <br></br>
+        </header>
+        <main>
+        <Typography variant="subtitle1" className={classes.appIntrotitle}>ブロスタのインストール</Typography>
           <div>
-            <Paper elevation={2} style={{backgroundColor: "#eeeeff"}}>
-              <Typography>
-                
-              </Typography>
+            <Paper elevation={2} style={{backgroundColor: "#eeeeff"}} className={classes.appIntro}>
+            <Typography variant="subtitle1" className={classes.appIntrotitle}>ブロスタのインストール</Typography>
               <br></br>
-              <Grid container spacing={2} alignContent="center">
+              <Grid container spacing={0} alignContent="center" style={{width: "85%"}}>
                 <Grid container item style={{width: "50%"}}>
-                  <Typography>ブロスタ</Typography>
-                  <img src="/buroicon256.jpg" alt="ひょうご防災ネット アプリアイコン" style={{width: "160px"}}/>
+                  <img src="/buroicon256.jpg" alt="ブロスタ アプリアイコン" style={{width: "90px", height:"90px"}}/>
                 </Grid>
                 <Grid container item alignContent="center" style={{width: "50%"}}>
                   <Container fixed>
                     <Grid item>
                       <Link href="https://play.google.com/store/apps/details?id=com.supercell.brawlstars&hl=ja&gl=US">
-                        <img src="/img/pages/Home/hyogo_bousai_google_play_badge.png" alt="Google Playリンク" style={{width: "160px", margin: "-10px"}}/>
+                        <img src="/img/pages/Home/hyogo_bousai_google_play_badge.png" alt="Google Playリンク" style={{width: "130px", margin: "-8px"}}/>
                       </Link>
                     </Grid>
                     <Grid container item>
                       <Link href="https://apps.apple.com/jp/app/%E3%83%96%E3%83%AD%E3%82%B9%E3%82%BF/id1229016807">
-                        <img src="/img/pages/Home/hyogo_bousai_app_store_black.svg" alt="App Storeリンク" style={{width: "140px"}}/>
+                        <img src="/img/pages/Home/hyogo_bousai_app_store_black.svg" alt="App Storeリンク" style={{width: "115px"}}/>
                       </Link>
                     </Grid>
                   </Container>
